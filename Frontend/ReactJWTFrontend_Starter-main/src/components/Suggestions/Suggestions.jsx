d22fce2e6dbe1e8fb2c33a0ba3d389519d665a07
ChaseLoeffler <chaselo609@gmail.com>
2023-12-02 22:20:29 -0500

    Added necessary async functions to suggestions component.

--- Frontend/ReactJWTFrontend_Starter-main/src/components/Suggestions/Suggestions.jsx
+++ Frontend/ReactJWTFrontend_Starter-main/src/components/Suggestions/Suggestions.jsx
@@ -3,10 +3,13 @@ import axios from 'axios';
 
 const Suggestions = (props) => {
     const [favDrinks, setFav] = useState();
+    const [cocktailInfo,setInfo] = useState();
+    const [userComs, setComs] = useState();
 
     useEffect(() => {
         getFavorites();
-    },[])
+        getUserComments();
+    },[],[])
 
     async function getFavorites(){
         try{
@@ -21,6 +24,36 @@ const Suggestions = (props) => {
         }
     }
 
+    async function cocktailDetails(cId){
+        try{
+            const controller = new AbortController();
+            let response = await axios.get(`https://the-cocktail-db.p.rapidapi.com/lookup.php?i=${cId}`, {
+              headers: {
+                'X-RapidAPI-Key': '83fc370ec5mshfafd14dfd77a29fp100ab0jsn61bda3159568',
+                'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
+              },
+            });
+            console.log(response.data)
+            setInfo(response.data)
+            return () => controller.abort();
+        } catch (err) {
+            console.log(err.message)
+        }
+    }
+
+    async function getUserComments(){
+        try{
+            const controller = new AbortController();
+            let resp = await axios.get(`https://localhost:5001/api/Comments`, {headers: {
+                Authorization: "Bearer " + token || null
+            }});
+            console.log(resp.data)
+            setComs(resp.data)
+            return () => controller.abort();
+        }catch(err){
+            console.log(err.message);
+        }
+    }
 
     return (
         <div>
